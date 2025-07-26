fa7fa446719979a27151df0c85a5d9c28b695eb6
markdrahg <markdrah54@gmail.com>
2025-07-26 18:08:38 +0000

    oganized public dir

--- User_dashboard/v2/README.md
+++
@@ -1,7 +0,0 @@
-# Dashboard
-
-Responsive Multi-Pages Dashboard using Pure Html , CSS and js
-
-# Demo:
-
-https://thunderboltforever.github.io/Dashboard/
--- js/loader.js
+++
@@ -1,8 +0,0 @@
-  window.addEventListener('load', () => {
-    const preloader = document.getElementById('preloader');
-
-    // Delay before fade-slide animation starts
-    setTimeout(() => {
-      preloader.classList.add('fade-out');
-    }, 1500);
-  });
